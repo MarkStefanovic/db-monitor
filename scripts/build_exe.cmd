119c4620fd8906e6ddf8167acf5f03ccae177150
markstefanovic <mark.stefanovic@gmail.com>
2021-04-26 09:44:39 -0700

    add build scripts

--- scripts/build_exe.cmd
+++ scripts/build_exe.cmd
@@ -5,10 +5,7 @@ for %%i in ("%~dp0..") DO SET "folder=%%~fi"
 del /S /Q "%folder%\dist\"
 mkdir "%folder%\dist\assets\"
 mkdir "%folder%\dist\log\"
-::mkdir "%folder%\dist\platforms\"
 poetry run pyinstaller "%folder%\%project%.spec" --distpath %folder%\dist\ --specpath %folder% --workpath %folder%\build
-::poetry run pyinstaller "%folder%\src\main.py" -F -n "%project%" -i "%folder%\assets\icons\app.ico" --distpath %folder%\dist\ --specpath %folder% --workpath %folder%\build --hidden-import PyQt5.sip
-::poetry run pyinstaller "%folder%\src\main.py" -F -w -n "%project%" -i "%folder%\assets\icons\app.ico" --distpath %folder%\dist\ --specpath %folder% --workpath %folder%\build
 ECHO F | xcopy /Y "%folder%\config.json" "%folder%\dist\config.json"
 xcopy /Y /E "%folder%\assets" "%folder%\dist\assets"
-::xcopy /Y /E "%folder%\.venv\Lib\site-packages\PyQt5\Qt\plugins\platforms" "%folder%\dist\platforms"
+xcopy /Y /E "%folder%\.venv\Lib\site-packages\PyQt5\Qt\plugins\platforms" "%folder%\dist\platforms"
